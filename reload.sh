1710beddcef22758bdb684883161502836f2ad05
Hicham MALKI <hicham.malki@lemonlearning.fr>
2023-03-14 10:15:06 +0100

    last config

--- reload.sh
+++ reload.sh
@@ -1,5 +1,6 @@
 
 docker exec -it app sh -c "
     git pull origin main --rebase;    
-    pm2 reload all; 
+    npm i;
+    export NODE_ENV=production&& pm2 reload all; 
 "
--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 clear;
+mkdir -p containers/app;
+mkdir -p containers/ssh;
 cd containers/app;
-git pull origin main --rebase;
+git pull origin main --rebase; # tag
 npm i;
+export NODE_ENV=production;
 cd ../..;
-sudo docker-compose up -d --build;
+sudo docker-compose up -d # --build;
